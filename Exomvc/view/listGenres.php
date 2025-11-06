87d5494f392b6f3b4f76ec53701d1d11cda092b2
Quentin Maia <q.maia@laposte.net>
2025-11-06 09:22:47 +0100

    Update listGenres.php

--- Exomvc/view/listGenres.php
+++ Exomvc/view/listGenres.php
@@ -2,6 +2,7 @@
             foreach($requete->fetchAll() as $genre) { ?>
                 <br>
                 <li class="genre"><a href="index.php?action=listFilmsByGenre&id=<?=$genre['id_genre']?>"><?=$genre["wording"]?></a></li>
-                <li class="supprgenre"><a style = "color = 'red' "href="index.php?action=deleteGenre&id=<?= $genre['id_genre'] ?>">Supprimer</a></li>
+                <li class="supprgenre"><a"href="index.php?action=deleteGenre&id=<?= $genre['id_genre'] ?>">Supprimer</a></li>
         <?php } ?>
+
         
