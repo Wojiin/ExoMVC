e1ed84155e625164df45a33e35495633bce0444a
Quentin Maia <q.maia@laposte.net>
2025-11-07 13:56:43 +0100

    Fix PHP syntax in listGenres.php

--- Exomvc/view/listGenres.php
+++ Exomvc/view/listGenres.php
@@ -2,7 +2,8 @@
             foreach($requete->fetchAll() as $genre) { ?>
                 <br>
                 <li class="genre"><a href="index.php?action=listFilmsByGenre&id=<?=$genre['id_genre']?>"><?=$genre["wording"]?></a></li>
-                <li class="supprgenre"><a"href="index.php?action=deleteGenre&id=<?= $genre['id_genre'] ?>">Supprimer</a></li>
+                <li class="supprgenre"><a href="index.php?action=deleteGenre&id=<?= $genre['id_genre'] ?>">Supprimer</a></li>
         <?php } ?>
 
         
+
